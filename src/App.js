7f7ed3c51d621d0cc5935efae23e90962522f15a
Jh0n16 <57267877+Jh0n16@users.noreply.github.com>
2024-09-16 19:24:05 -0300

    Enviando ao repositório remoto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Content, Row } from "./styles";
+
+import Input from './components/Input'
+import Button from './components/Button'
+
+import { useState } from "react";
 
 function App() {
+  const [currentNumber, setCurrentNumber] = useState('0')
+  const [firstNumber, setFirstNumber] = useState('0')
+  const [operation, setOperation] = useState('')
+
+  const handlerClear = () => {
+    setCurrentNumber('0')
+    setFirstNumber('0')
+    setOperation('')
+  }
+
+  const handlerAddNumber = (number) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`)
+  }
+
+  const handlerSumNumbers = () => {
+    if(firstNumber === '0') {
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('+')
+
+    } else {
+      const soma = Number(firstNumber) + Number(currentNumber)
+      setCurrentNumber(String(soma))
+      setOperation('')
+
+    }
+  }
+
+  const handlerSubNumbers = () => {
+    if(firstNumber === '0') {
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('-')
+
+    } else {
+      const sub = Number(firstNumber) - Number(currentNumber)
+      setCurrentNumber(String(sub))
+      setOperation('')
+
+    }
+  }
+
+  const handlerMultNumbers = () => {
+    if(firstNumber === '0') {
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('*')
+
+    } else {
+      const mult = Number(firstNumber) * Number(currentNumber)
+      setCurrentNumber(String(mult))
+      setOperation('')
+    }
+  }
+
+  const handlerDivNumbers = () => {
+    if(firstNumber === '0') {
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('/')
+
+    } else {
+      const div = Number(firstNumber) / Number(currentNumber)
+      setCurrentNumber(String(div))
+      setOperation('')
+    }
+  }
+
+  const handlerEquals = () => {
+    if(firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
+      switch(operation) {
+        case '+':
+          handlerSumNumbers()
+          break
+        
+        case '-':
+          handlerSubNumbers()
+          break
+        
+        case '*':
+          handlerMultNumbers()
+          break
+        
+        case '/':
+          handlerDivNumbers()
+          break
+
+        default:
+          break
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+          <Button label="#"/>
+          <Button label="/" onClick={handlerDivNumbers}/>
+          <Button label="*" onClick={handlerMultNumbers}/>
+          <Button label="C" onClick={handlerClear}/>
+        </Row>  
+        <Row>
+          <Button label="7" onClick={() => handlerAddNumber('7')}/>
+          <Button label="8" onClick={() => handlerAddNumber('8')}/>
+          <Button label="9" onClick={() => handlerAddNumber('9')}/>
+          <Button label="-" onClick={handlerSubNumbers}/>
+        </Row>  
+        <Row>
+          <Button label="4" onClick={() => handlerAddNumber('4')}/>
+          <Button label="5" onClick={() => handlerAddNumber('5')}/>
+          <Button label="6" onClick={() => handlerAddNumber('6')}/>
+          <Button label="+" onClick={handlerSumNumbers}/>
+        </Row>  
+        <Row>
+          <Button label="1" onClick={() => handlerAddNumber('1')}/>
+          <Button label="2" onClick={() => handlerAddNumber('2')}/>
+          <Button label="3" onClick={() => handlerAddNumber('3')}/>
+          <Button label="=" onClick={handlerEquals}/>
+        </Row>
+        <Row>
+          <Button label="0" onClick={() => handlerAddNumber('0')}/>
+        </Row>
+      </Content>
+    </Container>
   );
 }
 
---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.div`
+    padding: 20px;
+    border: 1px solid #000;
+    background-color: #222;
+    color: #fff;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+        cursor: pointer;    
+    }
+
+`
